b976e5ec098b5a8405d9d4a15901d52aad0e0275
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-08-16 10:06:18 -0300

    Initial commit

---
+++ app/src/main/java/com/spitzer/district8news/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.spitzer.district8news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spitzer.district8news.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
