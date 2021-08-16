2821ec0521aeeb5c6177919905b4179d66326cd9
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-08-16 10:37:03 -0300

    - added navigation file

--- app/src/main/java/com/spitzer/district8news/MainActivity.kt
+++ app/src/main/java/com/spitzer/district8news/MainActivity.kt
@@ -2,17 +2,33 @@ package com.spitzer.district8news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.spitzer.district8news.databinding.MainActivityBinding
 import com.spitzer.district8news.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun showProgressBar() {
+        binding.clProgressBar.visibility = View.VISIBLE
+        binding.coordinator.alpha = DIM_BACKGROUND_ALPHA
+    }
+
+    fun hideProgressBar() {
+        binding.clProgressBar.visibility = View.GONE
+        binding.coordinator.alpha = VISIBLE_BACKGROUND_ALPHA
+    }
+
+    companion object {
+        const val DIM_BACKGROUND_ALPHA = 0.75f
+        const val VISIBLE_BACKGROUND_ALPHA = 1f
     }
 }
