63a22f4b90df719e10e7f08aa9a82cae09a24e85
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-08-16 16:14:53 -0300

    Implemented MainFragment and MainViewModel

--- app/src/main/java/com/spitzer/district8news/ui/main/MainFragment.kt
+++ app/src/main/java/com/spitzer/district8news/ui/main/MainFragment.kt
@@ -1,32 +1,112 @@
 package com.spitzer.district8news.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.spitzer.district8news.R
+import com.spitzer.district8news.core.BaseFragment
+import com.spitzer.district8news.core.repository.data.Post
+import com.spitzer.district8news.databinding.MainFragmentBinding
+import com.spitzer.district8news.postadapter.PostAdapter
+import com.squareup.picasso.Picasso
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainFragment : Fragment() {
+@AndroidEntryPoint
+class MainFragment : BaseFragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var postAdapter: PostAdapter
+    private lateinit var fallbackDrawableImage: Drawable
+
+    private lateinit var bottomSheetDialog: BottomSheetDialog
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun obtainViewModel() = viewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        setupView()
+        defineObservables()
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun defineObservables() {
+        viewModel.posts.observe(viewLifecycleOwner, {
+            it?.getContentIfNotHandled()?.let { posts ->
+                if (posts.isEmpty()) {
+                    // TODO show no content layout
+                } else {
+                    try {
+                        Picasso.get()
+                            .load(posts.first().featuredMedia.first().href)
+                            .placeholder(R.drawable.ic_placeholder_image_24)
+                            .error(R.drawable.ic_broken_image_24)
+                            .into(binding.featuredPostImage)
+                    } catch (e: Exception) {
+                        binding.featuredPostImage.setImageDrawable(fallbackDrawableImage)
+                    }
+                    binding.featuredPostTitle.text = posts.first().title.rendered
+
+                    binding.featuredPostImage.setOnClickListener {
+                        viewModel.onPostClicked(1)
+                    }
+                    binding.featuredPostTitle.setOnClickListener {
+                        viewModel.onPostClicked(1)
+                    }
+                    binding.featuredPostTitleBackground.setOnClickListener {
+                        viewModel.onPostClicked(1)
+                    }
+
+                    postAdapter.updateData(posts.slice(IntRange(1, posts.size - 1)) as ArrayList<Post>)
+                }
+            }
+        })
     }
 
+    private fun setupView() {
+        fallbackDrawableImage = resources.getDrawable(R.drawable.ic_broken_image_24)
+
+        postAdapter = PostAdapter(fallbackDrawableImage)
+        postAdapter.onItemClickFunction { position -> viewModel.onPostClicked(position) }
+        binding.postsRecyclerView.apply {
+            layoutManager =
+                LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = postAdapter
+        }
+
+        bottomSheetDialog = BottomSheetDialog(requireContext())
+
+        binding.toolbarFilterTextView.setOnClickListener {
+
+            val view = layoutInflater.inflate(R.layout.categories_filter, null)
+            val btnClose = view.findViewById<ImageView>(R.id.closeBottomsheet)
+            btnClose.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+            bottomSheetDialog.setCancelable(false)
+            bottomSheetDialog.setContentView(view)
+            bottomSheetDialog.show()
+        }
+
+        viewModel.getInitialConfiguration()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/spitzer/district8news/ui/main/MainViewModel.kt
+++ app/src/main/java/com/spitzer/district8news/ui/main/MainViewModel.kt
@@ -1,7 +1,67 @@
 package com.spitzer.district8news.ui.main
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.spitzer.district8news.R
+import com.spitzer.district8news.core.BaseViewModel
+import com.spitzer.district8news.core.Event
+import com.spitzer.district8news.core.network.ResultData
+import com.spitzer.district8news.core.repository.PostRepository
+import com.spitzer.district8news.core.repository.data.Category
+import com.spitzer.district8news.core.repository.data.Post
+import com.spitzer.district8news.utils.extensions.filterCategories
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: PostRepository
+) : BaseViewModel() {
+
+    private val _categories = MutableLiveData<ArrayList<Category>>()
+    val categories: LiveData<ArrayList<Category>> = _categories
+
+    private val _posts = MutableLiveData<Event<ArrayList<Post>>>()
+    val posts: LiveData<Event<ArrayList<Post>>> = _posts
+
+    fun getInitialConfiguration() {
+        _loading.value = Event(true)
+        viewModelScope.launch {
+            fetchInitialData()
+        }
+    }
+
+    private suspend fun fetchInitialData() {
+
+        val categoriesResult = repository.getCategories()
+        val postsResult = repository.getPosts()
+
+        if (categoriesResult is ResultData.Success && postsResult is ResultData.Success) {
+            _loading.value = Event(false)
+            if (categoriesResult.data == null || postsResult.data == null) {
+                _snackbarError.value = Event(R.string.snackbar_could_not_fetch)
+            } else {
+                _categories.value = categoriesResult.data!!.filterCategories()
+                _posts.value = Event(postsResult.data!!)
+            }
+
+        } else {
+            _loading.value = Event(false)
+            if (categoriesResult.isNetworkError() || postsResult.isNetworkError()) {
+                _snackbarError.value = Event(R.string.snackbar_network_error)
+            } else {
+                _snackbarError.value = Event(R.string.snackbar_error)
+            }
+        }
+    }
+
+    fun onPostClicked(postPosition: Int) {
+
+    }
 }
